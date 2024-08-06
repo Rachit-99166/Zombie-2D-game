797cbfc54f26a5b3121f5b5d21aaf1a466a68003
RachitDuaCode <150435184+RachitDuaCode@users.noreply.github.com>
2024-08-06 20:08:10 +0530

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Transform locationLane1;
+    public Transform locationLane2;
+    public Transform locationLane3;
+    public Transform gunpoint;
+
+    Animator anim;
+
+    public GameObject[] Bullet;
+    public GameObject currentBullet;
+
+    private int laneSelect;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = new Vector2(locationLane2.position.x, locationLane2.position.y);
+        currentBullet = Bullet[0];//MaleBullet
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+             /* Player Movement Using Raycast */ 
+
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+
+            RaycastHit2D rayOut = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+            if (rayOut.collider != null)
+            {
+                /* Lane Detection */
+                if (rayOut.collider.name == "Lane")
+                {
+                    transform.position = new Vector2(locationLane1.position.x, locationLane1.position.y);
+                    laneSelect = 1;
+                }
+                else if (rayOut.collider.name == "Lane 2")
+                {
+                    transform.position = new Vector2(locationLane2.position.x, locationLane2.position.y);
+                    laneSelect = 2;
+                }
+                else if (rayOut.collider.name == "Lane 3")
+                {
+                    transform.position = new Vector2(locationLane3.position.x, locationLane3.position.y);
+                    laneSelect = 3;
+                }
+            }
+
+       
+
+        }
+
+    }
+
+    public void SetBullet(int index)
+    {
+        /* Select Bullet */
+        currentBullet = Bullet[index];
+    }
+
+    public void Fire()
+    {
+        if (this.anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Girl_Fire"))
+        {
+            return;
+        }
+        else
+        {
+            anim.SetTrigger("Fire");
+            Instantiate(currentBullet, gunpoint.transform.position, Quaternion.identity);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Land of Zombies
+
+Made an 2D game project using Unity where the objective is to defend against an oncoming horde of zombies. The game features zombies advancing in three distinct lanes, and the player must strategically kill them before they breach the defense line. The player has access to a variety of bullets, each specifically designed to be more effective against certain types of zombies. This adds an element of strategy, as selecting the appropriate ammunition is crucial for survival. Additionally, players can switch between lanes to position themselves optimally for taking down the zombies, making the gameplay dynamic and engaging.
+
+
+## 🛠 Skills
+
+C# and Assets has been used in this project.
+
+## Deployment
+To run game follow these steps:
+
+1.Ensure Unity is installed on your system.
+
+2.Install all necessary packages required for the project.
+
+3.Import the game project file into Unity.
+
+4.Start the game by clicking the "Play" button at the top of the Unity interface.
+
+5.To stop the game, click the "Pause" button.
+
+
+
+## Screenshots
+
+![Screenshot 2024-08-04 000447](https://github.com/user-attachments/assets/faede08e-2e48-4889-8466-8eadde092b9c)
